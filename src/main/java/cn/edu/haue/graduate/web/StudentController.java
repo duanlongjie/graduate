a5cbcd1857c90274d91f8f043c20f039ada613af
lnp1996 <1847824530@qq.com>
2018-06-20 21:22:58 +0800

    zai完善

--- src/main/java/cn/edu/haue/graduate/web/StudentController.java
+++ src/main/java/cn/edu/haue/graduate/web/StudentController.java
@@ -1,11 +1,9 @@
 package cn.edu.haue.graduate.web;
 
-import cn.edu.haue.graduate.entity.Grade;
 import cn.edu.haue.graduate.entity.ResultInfo;
 import cn.edu.haue.graduate.entity.Student;
 import cn.edu.haue.graduate.entity.StudentCreditResult;
 import cn.edu.haue.graduate.service.GraduateOrNotService;
-import cn.edu.haue.graduate.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,40 +13,23 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
-import java.util.List;
 
 /**
  * Author: lnp
  * Date: 2018/6/15
  **/
-@RequestMapping("/student")
 @Controller
 public class StudentController {
 
     @Resource
     GraduateOrNotService graduateOrNotService;
 
-    @Resource
-    StudentService studentService;
-
-    @RequestMapping("/student/graduationCon")
-    public String goToGraduationConditionsPage(HttpSession session, Model model) {
+    @RequestMapping("/student/graduationCon/{id}")
+    public String goToGraduationConditionsPage(@PathVariable String id,HttpSession session, Model model) {
         Student student= (Student) session.getAttribute("student");
-        StudentCreditResult studentCreditResult = graduateOrNotService.GraduateOrNot(student.getStudentId());
+        StudentCreditResult studentCreditResult = graduateOrNotService.GraduateOrNot(id);
         model.addAttribute("studentCreditResult", studentCreditResult);
         System.out.println(studentCreditResult);
         return "student/graduationConditions";
-    }
-
-    @RequestMapping("/student/grade")
-    public String goToGradePage(HttpSession session, Model model){
-        Student student= (Student) session.getAttribute("student");
-        List<Grade> gradeList = student.getGradeList();
-        model.addAttribute("gradeList",gradeList);
-        model.addAttribute("student",student);
-        System.out.println(gradeList);
-        return "/student/grade";
-
-
     }
 }
--- src/main/java/cn/edu/haue/graduate/web/StudentLoginController.java
+++ src/main/java/cn/edu/haue/graduate/web/StudentLoginController.java
@@ -3,8 +3,12 @@ package cn.edu.haue.graduate.web;
 import cn.edu.haue.graduate.constant.ResultCode;
 import cn.edu.haue.graduate.entity.ResultInfo;
 import cn.edu.haue.graduate.entity.Student;
+import cn.edu.haue.graduate.entity.StudentCreditResult;
+import cn.edu.haue.graduate.service.GraduateOrNotService;
 import cn.edu.haue.graduate.service.StudentLoginService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,35 +17,43 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
 @Controller
+@RequestMapping("/student")
 public class StudentLoginController {
 
     @Resource
     private StudentLoginService studentLoginService;
+    @Resource
+    GraduateOrNotService graduateOrNotService;
 
     //主页
-    @RequestMapping("/student/index")
+    @RequestMapping("/")
     public String studentToLogin(){
         return "student/login";
     }
 
     //登陆
-    @RequestMapping("/student/login")
-    public String studentLogin(@RequestParam("username")String studentId,@RequestParam("passwords")String passwords,HttpSession httpSession){
+    @RequestMapping("login")
+    public String studentLogin(@PathVariable("username") String studentId, @PathVariable("passwords")String passwords, HttpSession httpSession, Model model){
         System.out.println(studentId +"--" +passwords);
         ResultInfo<Student> studentResultInfo = studentLoginService.studentLogin(studentId, passwords);
         httpSession.setAttribute("student",studentResultInfo.getResultObj());
-        return "graduationCon";
+        //增加毕业条件数据
+        StudentCreditResult studentCreditResult = graduateOrNotService.GraduateOrNot(studentId);
+        model.addAttribute("studentCreditResult", studentCreditResult);
+        System.out.println(studentCreditResult);
+        return "student/graduationConditions";  //进入首页
+
     }
 
     //修改密码页面
-    @RequestMapping("/student/updatePage")
+    @RequestMapping("updatePage")
     public String updateStudent(){
         return "student/updatepwd";
     }
 
     //修改密码
     @ResponseBody
-    @RequestMapping("/student/update")
+    @RequestMapping("update")
     public ResultInfo<Student> studentUpdate(@RequestParam("username")String studentId,@RequestParam("oldpasswords")String oldpasswords,@RequestParam("passwords")String passwords){
         ResultInfo<Student> studentResultInfo = studentLoginService.studentUpdate(studentId, oldpasswords, passwords);
         return studentResultInfo;
