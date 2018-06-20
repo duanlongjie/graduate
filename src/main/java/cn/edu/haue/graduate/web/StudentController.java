f1773b2db1caecffa1f695d9c0008de69cae5e6f
王鑫 <957185402@qq.com>
2018-06-20 22:25:48 +0800

    稍稍修改

--- src/main/java/cn/edu/haue/graduate/web/StudentController.java
+++ src/main/java/cn/edu/haue/graduate/web/StudentController.java
@@ -1,9 +1,11 @@
 package cn.edu.haue.graduate.web;
 
+import cn.edu.haue.graduate.entity.Grade;
 import cn.edu.haue.graduate.entity.ResultInfo;
 import cn.edu.haue.graduate.entity.Student;
 import cn.edu.haue.graduate.entity.StudentCreditResult;
 import cn.edu.haue.graduate.service.GraduateOrNotService;
+import cn.edu.haue.graduate.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +16,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 /**
  * Author: lnp
@@ -25,8 +28,14 @@ public class StudentController {
     @Resource
     GraduateOrNotService graduateOrNotService;
 
+    @Resource
+    StudentService studentService;
+
     @RequestMapping("/student/graduationCon")
-    public ModelAndView goToGraduationConditionsPage(@RequestParam(value = "studentId") String id) {
+    public ModelAndView goToGraduationConditionsPage(@RequestParam(value = "studentId") String id,HttpSession session) {
+        ResultInfo resultInfo= studentService.getStudentById(id);
+        Student student = (Student) resultInfo.getResultObj();
+        session.setAttribute("student",student);
         ModelAndView modelAndView = new ModelAndView("/student/graduationConditions");
         StudentCreditResult studentCreditResult = graduateOrNotService.GraduateOrNot(id);
         modelAndView.addObject("studentCreditResult", studentCreditResult);
@@ -34,4 +43,16 @@ public class StudentController {
         return modelAndView;
     }
 
+
+    @RequestMapping("/grade")
+    public String goToGradePage(HttpSession session, Model model) {
+//        ResultInfo resultInfo= studentService.getStudentById(id);
+//        Student student = (Student) resultInfo.getResultObj();
+        Student student = (Student)session.getAttribute("student");
+        List<Grade> gradeList = student.getGradeList();
+        model.addAttribute("gradeList", gradeList);
+        model.addAttribute("student", student);
+        System.out.println(gradeList);
+        return "/student/grade";
+    }
 }
